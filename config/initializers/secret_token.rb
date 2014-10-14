06af8c68ca305d3a765e27ee66d55d1d2b368479
Juan Antonio <soft.jasa@hotmail.com>
2014-10-14 23:42:33 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistemaAlmacen::Application.config.secret_token = '8997db61a969d9bb6384f4c55d3c424ed2488c088a32809fe05a7617093cf1731274e11bc05840e49d932ac2f6706cf2be41ef8172d1637558b6a16984eba8fc'
